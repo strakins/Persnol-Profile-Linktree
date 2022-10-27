506547ca79ac80f57c90aa76adef515eac2810b0
unknown <akinolablessing25@gmail.com>
2022-10-27 09:23:15 +0100

    Footer Developement and Adding google fonts

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,11 +1,19 @@
-
+import Slack from '../assets/slack.png';
+import IGlogo from '../assets/I4G.png';
 
 function Footer() {
     return (
         <div className="footer">
-            <i className="fa-brands fa-slack social-icon"><a href='https://github.com/strakins' target='_blank' rel='noreferrer' > </a></i> 
-            <i className="fa-brands fa-github social-icon"><a href='https://github.com/strakins' target='_blank' rel='noreferrer' > </a></i> 
-            
+            <div className="footer_top">
+                <a href='#STRAKINS' target='_blank' rel='noreferrer' > <img src={Slack} alt="slack-icon" className='social-icons' /> </a>
+                <a href='https://github.com/strakins' target='_blank' rel='noreferrer' > <i className="fa-brands fa-github social-icon"> </i> </a>
+            </div>
+                <hr className='divider'/>
+            <div className='footerbase'>
+                <h4>Zuri<span className='dot'>.</span>Internship </h4>
+                <p className='footer-paragraph'>HNG Internship 9 Frontend Task</p>
+                <img src={IGlogo} alt="slack-icon" className='footer-icon' />
+            </div>
         </div>
     )
 }
