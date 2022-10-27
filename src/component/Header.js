1409c723ee3c71f5e4da1e2dcde93d2a6bcd9d94
unknown <akinolablessing25@gmail.com>
2022-10-27 23:41:17 +0100

    Share Button Added and button color adjusted

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,8 +1,11 @@
-
+import Share from '../assets/Icon.png'
+import Share2 from '../assets/Share2.png'
 
 const Header = () => {
     return (
         <div className="header">
+            <img src={Share} alt='share' className='share-button' />
+            <img src={Share2} alt='share' href='https://twitter.com/straksman' className='share-button-device' />
             <div className="header0">
                 <img src="https://raw.githubusercontent.com/strakins/My-Images/main/MY_PICS.jpg" alt="mypics"  id="profile_img"/>
             </div>
--- src/component/LinkItems.js
+++ src/component/LinkItems.js
@@ -5,22 +5,25 @@ function LinkItems() {
     return ( 
         <div>
             <ItemDisplay id="twitter">
-                <a href="https://twitter.com/straksman" target="_blank" rel="noreferrer" >Twitter Link <span className="span-text">Click Here to Find Books about Design and Coding Here </span> </a>
+                <a href="https://twitter.com/straksman" target="_blank" rel="noreferrer" >Twitter Link </a>
             </ItemDisplay>
+            <p id="slack" className='hidden'>
+                <a href="https://twitter.com/straksman" target="_blank" rel="noreferrer" >blessing_akinola</a>
+            </p>
             <ItemDisplay id="btn__zuri">
                 <a href="https://training.zuri.team" target="_blank" rel="noreferrer" id="">Zuri Team</a>
             </ItemDisplay>
             <ItemDisplay id="books">
-                <a href="http://books.zuri.team " target="_blank" rel="noreferrer">Zuri Books</a>
+                <a href="http://books.zuri.team " target="_blank" rel="noreferrer">See a  host of Exciting Books you will love!</a>
             </ItemDisplay>
             <ItemDisplay id="book_python">
-                <a href="https://books.zuri.team/python-for-beginners" target="_blank" rel="noreferrer">Python Books</a>
+                <a href="https://books.zuri.team/python-for-beginners" target="_blank" rel="noreferrer">Read Beginner Friendly Python Books</a>
             </ItemDisplay>
             <ItemDisplay id="pitch">
-                <a href="https://background.zuri.team" target="_blank" rel="noreferrer">Background Check for Coders <span className="span-text">Hire Best Developers Here </span></a>
+                <a href="https://background.zuri.team" target="_blank" rel="noreferrer">Need Tech Talents? Check Here..</a>
             </ItemDisplay>
             <ItemDisplay id="book_design">
-                <a href="https://books.zuri.team/design-rules" target="_blank" rel="noreferrer">Design Books <span className="span-text">Best Design Guides by Zuri</span></a>
+                <a href="https://books.zuri.team/design-rules" target="_blank" rel="noreferrer">Checkout a Host of Design Guide by Zuri Team</a>
             </ItemDisplay>
         </div>
     )
