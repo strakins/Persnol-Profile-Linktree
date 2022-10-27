a016b0ea76e471fcbc0ea2d7a30b4537513154b9
unknown <akinolablessing25@gmail.com>
2022-10-27 23:47:11 +0100

    Share Button Functionality

--- src/component/Header.js
+++ src/component/Header.js
@@ -4,8 +4,9 @@ import Share2 from '../assets/Share2.png'
 const Header = () => {
     return (
         <div className="header">
-            <img src={Share} alt='share' className='share-button' />
-            <img src={Share2} alt='share' href='https://twitter.com/straksman' className='share-button-device' />
+            
+            <a href='https://twitter.com/straksman' target="_blank" rel="noreferrer"> <img src={Share} alt='share' className='share-button' /> </a>
+            <a href='https://twitter.com/straksman' target="_blank" rel="noreferrer"> <img src={Share2} alt='share' className='share-button-device' /> </a>
             <div className="header0">
                 <img src="https://raw.githubusercontent.com/strakins/My-Images/main/MY_PICS.jpg" alt="mypics"  id="profile_img"/>
             </div>
